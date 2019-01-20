13e9eca3ae3b2e50f380d79ae5ba18a80109088d
Kelvin Fichter <kelvinfichter@gmail.com>
2019-01-20 14:58:17 -0500

    Add way to stop the node

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
-const Plasma = require('plasma-core')
+const PlasmaCore = require('plasma-core')
 const RPCServerService = require('./services/rpc-server-service')
 
 class PlasmaApp {
   constructor () {
-    this.plasma = new Plasma({
+    this.core = new PlasmaCore({
       contract: {
         abi: '',
         address: '0x0'
       },
     })
 
-    this.plasma.registerService(RPCServerService, {
+    this.core.registerService(RPCServerService, {
       port: '9898'
     })
   }
 
-  start () {
-    this.plasma.startServices()
+  async start () {
+    return this.core.startServices()
+  }
+
+  async stop () {
+    return this.core.stopServices()
   }
 }
 
