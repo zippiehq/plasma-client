e1066a82b2ffcc147b0753cb4c78f636636f4340
Kelvin Fichter <kelvinfichter@gmail.com>
2019-01-30 00:40:40 -0500

    Add more logs to node.js

--- node.js
+++ node.js
@@ -3,21 +3,47 @@
 const program = require('commander')
 const PlasmaCore = require('plasma-core')
 const PlasmaNode = require('./index')
+const pkg = require('./package.json')
 
 program
   .version('0.0.1')
   .option('-o, --operator <endpoint>', 'Endpoint of the operator to connect to.', 'http://localhost:3000')
   .option('-e, --ethereum <endpoint>', 'Endpoint of the Ethereum node to connect to.', 'http://localhost:8545')
+  .option('-h, --hostname <hostname>', 'Host to run the node on.', 'localhost')
   .option('-p, --port <port>', 'Port to run the node on.', '9898')
   .parse(process.argv)
 
 const options = {
+  logger: { log: () => { return true } },
   port: program.port,
   ethereumEndpoint: program.ethereum,
   contractProvider: new PlasmaCore.providers.ContractProviders.HttpContractProvider(),
   walletProvider: new PlasmaCore.providers.WalletProviders.Web3WalletProvider(),
-  operatorProvider: new PlasmaCore.providers.OperatorProviders.HttpOperatorProvider({ url: program.endpoint })
+  operatorProvider: new PlasmaCore.providers.OperatorProviders.HttpOperatorProvider({ url: program.operator })
 }
 
 const node = new PlasmaNode(options)
-node.start()
+const client = node.client
+
+const getSectionTitle = (title) => {
+  return '\n' + title + '\n' + '='.repeat(title.length)
+}
+
+const start = async () => {
+  await node.start()
+  console.log('Plasma Node v' + pkg.version)
+
+  console.log(getSectionTitle('Available Accounts'))
+  const accounts = await client.getAccounts()
+  accounts.forEach((account, i) => {
+    console.log(`(${i}) ${account}`)
+  })
+
+  console.log(getSectionTitle('Node Information'))
+  console.log(`Operator: ${program.operator}`)
+
+  console.log(getSectionTitle('Logs'))
+  console.log('Node started successfully')
+  console.log(`Node listening on http://${program.hostname}:${program.port}`)
+}
+start()
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const PlasmaCore = require('plasma-core')
+const PlasmaClient = require('plasma-js-lib')
 const RPCServerService = require('./services/rpc-server-service')
 
 const defaultOptions = {
@@ -16,6 +17,7 @@ class PlasmaNode {
     this.core.registerService(RPCServerService, {
       port: options.port
     })
+    this.client = new PlasmaClient(new PlasmaClient.providers.HttpProvider(`http://localhost:${options.port}`))
   }
 
   /**
@@ -34,6 +36,18 @@ class PlasmaNode {
     return this.core.stop()
   }
 
+  /**
+   * Logs a message using the current logger.
+   * @param {string} message Message to be logged.
+   */
+  log (message) {
+    this.core.logger.log(message)
+  }
+
+  /**
+   * Sets the plasma chain contract's address.
+   * @param {string} address New contract address.
+   */
   setContractAddress (address) {
     this.core.services.contract.contract.options.address = address
   }
