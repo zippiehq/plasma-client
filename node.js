bd7f598ff32709e80fe1f3468e3466f020cd4a4c
Kelvin Fichter <kelvinfichter@gmail.com>
2019-02-08 14:22:54 -0500

    Small fixes

--- node.js
+++ node.js
@@ -10,12 +10,13 @@ const pkg = require('./package.json')
 
 program
   .version('0.0.1')
-  .option('-o, --operator <endpoint>', 'Endpoint of the operator to connect to.', 'https://operator.plasma.group/api')
+  .option('-c, --chain <name>', 'Name of the plasma chain to connect to.', 'PG-beta.8')
   .option('-e, --ethereum <endpoint>', 'Endpoint of the Ethereum node to connect to.', 'https://rinkeby.infura.io/v3/fce31f1fb2d54caa9b31ed7d28437fa5')
   .option('-h, --hostname <hostname>', 'Host to run the client on.', 'localhost')
-  .option('-w, --wallet <wallet>', 'Which wallet to use. Either "remote" or "local".', 'local')
   .option('-p, --port <port>', 'Port to run the client on.', '9898')
-  .option('-d, --debug', 'Runs the node in debug mode')
+  .option('-w, --wallet <mode>', 'Which wallet mode to use. Either "remote" or "local".', 'local')
+  .option('-d, --debug', 'Runs the node in debug mode.')
+  .option('-f, --finality <blocks>', 'Number of blocks to wait before considering events final.', parseInt, 0)
   .parse(process.argv)
 
 const dbPath = path.join(__dirname, '/chaindb/')
@@ -28,16 +29,16 @@ const wallets = {
 const debug = program.debug ? 'debug:*' : ''
 
 const options = {
-  finalityDepth: 0,
+  finalityDepth: program.finality,
   port: program.port,
   ethereumEndpoint: program.ethereum,
-  operatorEndpoint: program.operator,
   dbPath: dbPath,
   debug: `service:*,${debug}`,
   contractProvider: PlasmaCore.providers.ContractProviders.ContractProvider,
   walletProvider: wallets[program.wallet],
   operatorProvider: PlasmaCore.providers.OperatorProviders.HttpOperatorProvider,
-  dbProvider: PlasmaCore.providers.DBProviders.LevelDBProvider
+  dbProvider: PlasmaCore.providers.DBProviders.LevelDBProvider,
+  plasmaChainName: program.chain
 }
 
 const node = new PlasmaNode(options)
@@ -72,7 +73,7 @@ const getSectionTitle = (title) => {
   })
 
   console.log(getSectionTitle('Client Information'))
-  console.log(`Operator: ${program.operator}`)
+  console.log(`Plasma Chain: ${program.chain}`)
   console.log(`Ethereum Node: ${program.ethereum}`)
   console.log(`Listening on: http://${program.hostname}:${program.port}`)
 
