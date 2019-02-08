a57265b3aee782748114306ae4ebdfd9473289aa
Kelvin Fichter <kelvinfichter@gmail.com>
2019-02-08 14:38:54 -0500

    Cleanup and version bump

--- cli.js
+++ cli.js
@@ -5,85 +5,101 @@ const colors = require('colors')
 const program = require('commander')
 const web3Utils = require('web3-utils')
 
+program
+  .version('0.0.1')
+  .option('-h, --hostname <hostname>', 'Host the node is running on. Defaults to 127.0.0.1.', '127.0.0.1')
+  .option('-p, --port <port>', 'Port the node is running on. Defaults to 9898.', '9898')
+
+const client = () => new Plasma(`http://${program.hostname}:${program.port}`)
+
+/**
+ * Converts an account index into an address.
+ * @param {number} account Account index.
+ * @return {string} Account address.
+ */
 const parseAccount = async (account) => {
   if (!web3Utils.isAddress(account)) {
-    const accounts = await createClient().getAccounts()
+    const accounts = await client().getAccounts()
     account = accounts[account]
   }
   return account
 }
 
 program
-  .version('0.0.1')
-  .option('-h, --hostname <hostname>', 'Host the node is running on. Defaults to 127.0.0.1.', '127.0.0.1')
-  .option('-p, --port <port>', 'Port the node is running on. Defaults to 9898.', '9898')
-  .option('-o, --operator <operator>', 'URL of the operator api. Defaults to http://localhost:3000/api.', 'http://localhost:3000/api')
-
-const createClient = () => new Plasma(
-  new Plasma.providers.HttpProvider({endpoint: `http://${program.hostname}:${program.port}`})
-)
-const createOperator = () => new Plasma.PlasmaOperator(program.operator)
-
-program.command('listaccounts').action(async () => {
-  const accounts = await createClient().getAccounts()
-  accounts.forEach((account, i) => {
-    const maxDigits = (accounts.length - 1).toString().length
-    const accountNumber = i.toString().padStart(maxDigits, '0')
-    console.log(`(${accountNumber}) ${account}`)
+  .command('listaccounts')
+  .description('lists all available accounts')
+  .action(async () => {
+    const accounts = await client().getAccounts()
+    accounts.forEach((account, i) => {
+      const maxDigits = (accounts.length - 1).toString().length
+      const accountNumber = i.toString().padStart(maxDigits, '0')
+      console.log(`(${accountNumber}) ${account}`)
+    })
   })
-})
 
-program.command('getaccount <account>').action(async (account) => {
-  account = await parseAccount(account)
-  console.log(account)
-})
+program
+  .command('getaccount <index>')
+  .description('returns the address of the account with the given index')
+  .action(async (index) => {
+    const account = await parseAccount(index)
+    console.log(account)
+  })
 
-program.command('createaccount').action(async () => {
-  const address = await createClient().createAccount()
-  console.log(`Created new account: ${address}`)
-})
+program
+  .command('createaccount')
+  .description('creates a new account')
+  .action(async () => {
+    const address = await client().createAccount()
+    console.log(`Created new account: ${address}`)
+  })
 
-program.command('getbalance <account>').action(async (account) => {
-  account = await parseAccount(account)
-  const balances = await createClient().getBalances(account)
+program
+  .command('getbalance <account>')
+  .description('returns the balance of an account')
+  .action(async (account) => {
+    account = await parseAccount(account)
+    const balances = await client().getBalances(account)
 
-  if (Object.keys(balances).length === 0) {
-    console.log('Account has no balance')
-    return
-  }
+    if (Object.keys(balances).length === 0) {
+      console.log('Account has no balance')
+      return
+    }
 
-  console.log('Balances:')
-  for (let token in balances) {
-    const balance = balances[token]
-    console.log(`${token}: ${balance.toString()}`)
-  }
-})
+    console.log('Balances:')
+    for (let token in balances) {
+      const balance = balances[token]
+      console.log(`${token}: ${balance.toString()}`)
+    }
+  })
 
 program
   .command('deposit <account> <token> <amount>')
+  .description('submits a deposit for an account')
   .action(async (account, token, amount) => {
     account = await parseAccount(account)
     console.log(`Sending deposit transaction...`)
-    const result = await createClient().deposit(token, amount, account)
+    const result = await client().deposit(token, amount, account)
     console.log('Deposit successful!')
     console.log(`View deposit on Etherscan: https://rinkeby.etherscan.io/tx/${result.transactionHash}`)
   })
 
 program
   .command('send <from> <to> <token> <amount>')
+  .description('sends tokens from one account to another')
   .action(async (from, to, token, amount) => {
     from = await parseAccount(from)
     to = await parseAccount(to)
-    const receipt = await createClient().sendTransaction(from, to, token, amount)
+    const receipt = await client().sendTransaction(from, to, token, amount)
     console.log(`Transaction receipt: ${receipt}`)
   })
 
 program
   .command('exit <account> <token> <amount>')
+  .description('starts a withdrawal for an account')
   .action(async (account, token, amount) => {
     account = await parseAccount(account)
     console.log(`Sending exit transaction(s)...`)
-    const result = await createClient().startExit(account, token, amount)
+    const result = await client().startExit(account, token, amount)
 
     if (result.length === 0) return
     console.log(`Exited in ${result.length} transaction(s)`)
@@ -97,10 +113,11 @@ program
 
 program
   .command('finalizeexits <account>')
+  .description('finalizes any pending withdrawals for an account')
   .action(async (account) => {
     account = await parseAccount(account)
     console.log(`Sending exit finalization transaction(s)...`)
-    const result = await createClient().finalizeExits(account)
+    const result = await client().finalizeExits(account)
     console.log(`Finalized ${result.length} exit(s)`)
 
     if (result.length === 0) return
@@ -114,9 +131,10 @@ program
 
 program
   .command('getexits <account>')
+  .description('returns a list of pending withdrawals for an account')
   .action(async (account) => {
     account = await parseAccount(account)
-    const exits = await createClient().getExits(account)
+    const exits = await client().getExits(account)
     exits.forEach((exit, i) => {
       const maxDigits = (exits.length - 1).toString().length
       const exitNumber = i.toString().padStart(maxDigits, '0')
@@ -127,8 +145,10 @@ program
 
 program
   .command('submitblock')
+  .description('asks the operator to submit a block, used for testing')
   .action(async () => {
-    await createOperator().submitBlock()
+    const block = await client().submitBlock()
+    console.log(`Submitted block #${block}`)
   })
 
 if (!process.argv.slice(2).length) {
